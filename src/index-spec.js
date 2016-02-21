abf04f157fe05dc159fa660a79b716fee5602900
Joern Bernhardt <jb@campudus.com>
2016-02-21 10:31:31 +0100

    create agenda for tests
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/index-spec.js
+++ src/index-spec.js
@@ -8,15 +8,21 @@ describe('test setup', () => {
 
 });
 
+var KATAS_URL = 'http://katas.tddbin.com/katas/es6/language/__grouped__.json';
+var INVALID_URL = 'http://katas.tddbin.com/katas/es6/language/__all__.json';
+// 1) load it correctly
+// 2) load a wrong URL
+// 3) load wrong structured file
+
 function loadKatasJsonFrom() {
-  return Promise.resolve({groups:true});
+  return Promise.resolve({groups : true});
 }
 
 describe('loading the katas JSON', () => {
 
   it('works', () => {
     return loadKatasJsonFrom()
-      .then(function(katasJson) {
+      .then(function (katasJson) {
         assert('groups' in katasJson);
       });
   });
