4933f7d1e3e13576217bfadae03d00f7e154c20b
Joern Bernhardt <jb@campudus.com>
2016-02-21 10:45:05 +0100

    load json from url with bad test style
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/index-spec.js
+++ src/index-spec.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var fetch = require('node-fetch');
 
 describe('test setup', () => {
 
@@ -9,21 +10,36 @@ describe('test setup', () => {
 });
 
 var KATAS_URL = 'http://katas.tddbin.com/katas/es6/language/__grouped__.json';
+var WRONG_URL = 'http://wrong.url.test/katas.json';
 var INVALID_URL = 'http://katas.tddbin.com/katas/es6/language/__all__.json';
 // 1) load it correctly
 // 2) load a wrong URL
 // 3) load wrong structured file
 
-function loadKatasJsonFrom() {
-  return Promise.resolve({groups : true});
+function loadKatasJsonFrom(url) {
+  return fetch(url)
+    .then(function (res) {
+      return res.json();
+    });
 }
 
 describe('loading the katas JSON', () => {
 
   it('works', () => {
-    return loadKatasJsonFrom()
+    return loadKatasJsonFrom(KATAS_URL)
       .then(function (katasJson) {
         assert('groups' in katasJson);
+      });
+  });
+
+  it('fails when using a wrong URL', () => {
+    var count = 0;
+    return loadKatasJsonFrom(WRONG_URL)
+      .catch(function () {
+        count++;
+      })
+      .then(function () {
+        assert(count === 1);
       });
   });
 
