6d81ce785f5d0c719af7b3cf719e3b4979326370
Joern Bernhardt <jb@campudus.com>
2016-02-21 12:10:58 +0100

    Use template string for urls
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/index-spec.js
+++ src/index-spec.js
@@ -9,10 +9,11 @@ describe('test setup', () => {
 
 });
 
-const KATAS_URL = 'http://katas.tddbin.com/katas/es6/language/__grouped__.json';
+const getUrl = (str) => `http://katas.tddbin.com/katas/es6/language/__${str}__.json`;
+const KATAS_URL = getUrl('grouped');
+const INVALID_URL = getUrl('all');
 const WRONG_URL = 'no-url';
 const NON_EXISTENT_URL = 'http://wrong.url.test/katas.json';
-const INVALID_URL = 'http://katas.tddbin.com/katas/es6/language/__all__.json';
 // 1) load it correctly
 // 2) load a wrong URL
 // 3) load wrong structured file
