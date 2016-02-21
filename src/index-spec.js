88b3ad317dd18af97e882ebe9458547801a08aa8
Joern Bernhardt <jb@campudus.com>
2016-02-21 10:29:46 +0100

    load katas from json
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/index-spec.js
+++ src/index-spec.js
@@ -7,3 +7,18 @@ describe('test setup', () => {
   });
 
 });
+
+function loadKatasJsonFrom() {
+  return Promise.resolve({groups:true});
+}
+
+describe('loading the katas JSON', () => {
+
+  it('works', () => {
+    return loadKatasJsonFrom()
+      .then(function(katasJson) {
+        assert('groups' in katasJson);
+      });
+  });
+
+});
